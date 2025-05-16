ddfe1e8e80aae47f119802711b3d433898f8abd6
DaiHoiLun <minhdaimk111@gmail.com>
2025-05-16 12:02:07 +0700

    feat(Exception): init global exception handling

---
+++ BackEnd/Warehouse/src/main/java/com/ddd/warehouse/Exception/ErrorCode.java
@@ -0,0 +1,22 @@
+package com.ddd.warehouse.Exception;
+
+import lombok.*;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.HttpStatusCode;
+
+@Getter
+@NoArgsConstructor
+public enum ErrorCode {
+    INVALID_KEY(1001,"Invalid key", HttpStatus.BAD_REQUEST),
+
+    UNAUTHENTICATED(1004,"Khong du quyen truy cap",HttpStatus.UNAUTHORIZED),
+    UNCATEGORIZED(9999,"Uncategorized", HttpStatus.INTERNAL_SERVER_ERROR);
+    ErrorCode(int Code,String Message, HttpStatusCode sponse){
+        this.code = Code;
+        this.message = Message;
+        this.status = sponse;
+    }
+    private int code;
+    private String message;
+    private HttpStatusCode status;
+}
