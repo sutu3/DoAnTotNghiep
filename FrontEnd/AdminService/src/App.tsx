5934a644adcca3eab27679fe8e8529e73cbe3953
DaiHoiLun <minhdaimk111@gmail.com>
2025-05-16 11:32:32 +0700

    chore(Vite-Project): init vite-project project

---
+++ FrontEnd/AdminService/src/App.tsx
@@ -0,0 +1,14 @@
+
+import './App.css'
+import {Button} from "@/components/ui/button.tsx";
+
+function App() {
+
+  return (
+      <div className="flex flex-col items-center justify-center min-h-svh">
+          <Button className={"text-yellow-300"}>Click me</Button>
+      </div>
+  )
+}
+
+export default App
