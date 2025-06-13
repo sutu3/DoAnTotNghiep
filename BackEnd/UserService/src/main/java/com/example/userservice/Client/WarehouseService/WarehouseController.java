b3f67f1d6814cb59d7e711230169f2e2152cf7a2
DELL <minhdaimk111@gmail.com>
2025-06-13 12:36:01 +0700

    fix(Add Dockerfile, fix version java for project spring boot):

--- BackEnd/UserService/src/main/java/com/example/userservice/Client/WarehouseService/WarehouseController.java
+++ BackEnd/UserService/src/main/java/com/example/userservice/Client/WarehouseService/WarehouseController.java
@@ -10,7 +10,7 @@ import org.springframework.web.bind.annotation.RequestParam;
 
 @FeignClient(
         name = "Warehouse",
-        url = "https://warehouseservice-latest.onrender.com/api",
+        url = "https://doantotnghiep-pb6y.onrender.com/api",
         fallback = WarehouseServiceFallback.class)
 public interface WarehouseController {
     @GetMapping("/warehouses/{id}")
