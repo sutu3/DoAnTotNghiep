188649d97fb958b2874b39a6eb17880efbbaa8ad
DELL <minhdaimk111@gmail.com>
2025-06-16 13:05:18 +0700

    fix(Cros): add webConfig to Client  access to Api

---
+++ BackEnd/Warehouse/src/main/java/com/ddd/warehouse/Config/WebConfig.java
@@ -0,0 +1,17 @@
+package com.ddd.warehouse.Config;
+
+import org.springframework.context.annotation.Configuration;
+import org.springframework.web.servlet.config.annotation.CorsRegistry;
+import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
+
+@Configuration
+public class WebConfig implements WebMvcConfigurer {
+    @Override
+    public void addCorsMappings(CorsRegistry registry) {
+        registry.addMapping("/**") // Apply CORS policy to all endpoints
+                .allowedOrigins("http://localhost:5173") // Allowed origins
+                .allowedMethods("GET", "POST", "PUT", "DELETE", "OPTIONS") // HTTP methods
+                .allowedHeaders("Authorization", "Content-Type") // Allowed headers
+                .allowCredentials(true); // Allow credentials (cookies, Authorization headers, etc.)
+    }
+}
