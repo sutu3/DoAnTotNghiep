9ad9bf9750ee8c339bd96cefe8c81c81fa5f7fd4
DELL <minhdaimk111@gmail.com>
2025-06-16 15:35:45 +0700

    update

--- BackEnd/UserService/src/main/java/com/example/userservice/Config/WebConfig.java
+++ BackEnd/UserService/src/main/java/com/example/userservice/Config/WebConfig.java
@@ -1,4 +1,4 @@
-package com.ddd.warehouse.Config;
+package com.example.userservice.Config;
 
 import org.springframework.context.annotation.Configuration;
 import org.springframework.web.servlet.config.annotation.CorsRegistry;
