5439b6a3273c464c0cd64fe4637c7c737b55d9dc
DELL <minhdaimk111@gmail.com>
2025-07-03 14:13:13 +0700

    feat(WarehouseService):update Response Bin

--- BackEnd/Warehouse/src/main/java/com/ddd/warehouse/Dto/Response/Bin/BinResponse.java
+++ BackEnd/Warehouse/src/main/java/com/ddd/warehouse/Dto/Response/Bin/BinResponse.java
@@ -1,6 +1,7 @@
 package com.ddd.warehouse.Dto.Response.Bin;
 
 import com.ddd.warehouse.Dto.Response.Warehouse.WarehousesResponse;
+import com.ddd.warehouse.Enum.BinStatus;
 import com.ddd.warehouse.Module.Warehouses;
 import lombok.Builder;
 
@@ -11,6 +12,7 @@ public record BinResponse(
         String binId,
         String binCode,
         Integer capacity,
+        BinStatus status,
         WarehousesResponse warehouse,
         LocalDateTime createdAt,
         LocalDateTime updatedAt,
