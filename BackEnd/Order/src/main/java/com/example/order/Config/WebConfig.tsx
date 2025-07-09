f0a7e21735ee38b8b2b0489267566e2d32fea680
sutu3 <134842549+sutu3@users.noreply.github.com>
2025-07-09 11:58:41 +0700

    feat(Backend):Add config web security

---
+++ BackEnd/Order/src/main/java/com/example/order/Config/WebConfig.tsx
@@ -0,0 +1,18 @@
+package com.example.order.Config;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.data.web.config.EnableSpringDataWebSupport;
+import org.springframework.web.servlet.config.annotation.CorsRegistry;
+import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
+
+@Configuration
+@EnableSpringDataWebSupport(pageSerializationMode = EnableSpringDataWebSupport.PageSerializationMode.VIA_DTO)
+public class WebConfig implements WebMvcConfigurer {
+    @Override
+    public void addCorsMappings(CorsRegistry registry) {
+        registry.addMapping("/**") // Apply CORS policy to all endpoints
+                .allowedOrigins("http://localhost:5173") // Allowed origins
+                .allowedMethods("GET", "POST", "PUT", "DELETE", "OPTIONS") // HTTP methods
+                .allowedHeaders("*") // Allowed headers
+                .allowCredentials(true); // Allow credentials (cookies, Authorization headers, etc.)
+    }
+}
