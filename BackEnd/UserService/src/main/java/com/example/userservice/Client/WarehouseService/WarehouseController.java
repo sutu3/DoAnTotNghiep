c5e82c149700821b2c0623ee366f62506c9ad231
DELL <minhdaimk111@gmail.com>
2025-07-17 23:18:41 +0700

    feat(Backend): update security for user

--- BackEnd/UserService/src/main/java/com/example/userservice/Client/WarehouseService/WarehouseController.java
+++ BackEnd/UserService/src/main/java/com/example/userservice/Client/WarehouseService/WarehouseController.java
@@ -3,6 +3,7 @@ package com.example.userservice.Client.WarehouseService;
 import com.example.userservice.Client.WarehouseService.Dto.Responses.Warehouse.WarehousesResponse;
 import com.example.userservice.Client.WarehouseService.Fallbacks.WarehouseServiceFallback;
 import com.example.userservice.Dto.Responses.ApiResponse;
+import com.example.userservice.Security.AuthenticationRequestInterceptor;
 import org.springframework.cloud.openfeign.FeignClient;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
@@ -12,7 +13,9 @@ import org.springframework.web.bind.annotation.RequestParam;
         name = "Warehouse",
         url = "https://doantotnghiep-r5ta.onrender.com/api/cache",
         //url = "http://localhost:8081/api/cache",
-        fallback = WarehouseServiceFallback.class)
+        fallback = WarehouseServiceFallback.class,
+        configuration = {AuthenticationRequestInterceptor.class}
+)
 public interface WarehouseController {
     @GetMapping("/warehouses/{id}")
     ApiResponse<WarehousesResponse> getWarehouse(@PathVariable String id);
